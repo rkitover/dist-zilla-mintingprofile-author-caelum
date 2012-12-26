d732bdb0d9252577ba545ea17ca8dd8e26592b5b
Rafael Kitover <rkitover@cpan.org>
2012-12-26 06:28:47 -0500

    dzil minting profile for @AVAR
    The default profile in this MintingProfile provider will set up a dist
    for the AVAR PluginBundle. It fills out some POD, the dist.ini, Changes
    file and a .gitignore .
    
    In the future I'd like to make it create the github repo automatically
    as well.

---
+++ lib/Dist/Zilla/MintingProfile/Author/Caelum.pm
@@ -0,0 +1,42 @@
+use utf8;
+package Dist::Zilla::MintingProfile::Author::Caelum;
+
+use strict;
+use warnings;
+use 5.008001;
+
+use Moose;
+use Dist::Zilla::PluginBundle::AVAR ();
+
+with 'Dist::Zilla::Role::MintingProfile::ShareDir';
+
+=encoding UTF-8
+
+=head1 NAME
+
+Dist::Zilla::MintingProfile::Author::Caelum - Basic Minting Profile for @AVAR
+
+=head1 SYNOPSIS
+
+    dzil new -P Author::Caelum Your::Dist::Name
+
+=head1 DESCRIPTION
+
+This is a general purpose minting profile for
+L<Dist::Zilla::PluginBundle::AVAR>.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Dist::Zilla::PluginBundle::AVAR>
+
+=back
+
+=head1 AUTHOR
+
+Rafael Kitover <rkitover@cpan.org>
+
+=cut
+
+__PACKAGE__; # End of Dist::Zilla::MintingProfile::Author::Caelum
